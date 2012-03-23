fe7f69006b86f589d376b81ae91a8adf83284dd6
Robert Berry <rjberry@gmail.com>
2012-03-23 20:19:00 +0000

    Adding queries for index & post.

--- blog.py
+++ blog.py
@@ -5,23 +5,35 @@ Quick blog.
 __author__ = "Robert Berry"
 __email__ = "rjberry@gmail.com"
 
-
-
 from flask import Flask, url_for, render_template
 
 from blog.models import User, Post
 from blog.environment import Environment
 
+CONFIG_PATH = "config.yml"
+POSTS_PER_PAGE = 20
+
 app = Flask(__name__)
 
+@app.before_request:
+def before_request():
+    g.env = Environment(CONFIG_PATH)
+    g.session = g.env.create_database_session()
+
+@app.teardown_request:
+def teardown_request():
+    g.session.close()
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    posts = g.session.query(Post).join(User).order_by(Post.created) \
+        .limit(POSTS_PER_PAGE)
+    return render_template('index.html', posts=posts)
 
 @app.route("/post/<id>")
 def post(post_id):
-    pass
+    post = g.session.query(Post).filter_by(Post.id == int(post_id)).one()
+    return render_template('post.html', post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blog/models.py
+++ blog/models.py
@@ -5,30 +5,31 @@ Models.
 import sqlalchemy
 
 from sqlalchemy import Column, Integer, Unicode, ForeignKey, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy import func
 
 Base = declarative_base()
 
-class Post(Base):
+class Defaults(object):
+    @declared_attr
+    def __tablename__(cls):
+        # fairly dumb way of getting plural form of class name
+        return cls.__name__.lower() + "s"
 
-    __tablename__ = "posts"
-
+    # id is always this
     id = Column("id", Integer, primary_key=True)
+
+class Post(Base, Defaults):
     author_id = Column("author_id", Integer, ForeignKey("users.id"))
     title = Column("title", Unicode)
     body = Column("body", Unicode)
-    created = Column("created", DateTime)
-    modified = Column("modified", DateTime)
+    created = Column("created", DateTime, default=func.now())
+    modified = Column("modified", DateTime, default=func.now())
 
     author = relationship("User", backref=backref("posts", order_by=created))
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column("id", Integer, primary_key=True)
+class User(Base, Defaults):
     user_name = Column("user_name", Unicode)
     first_name = Column("first_name", Unicode)
     last_name = Column("last_name", Unicode)
-
-    
